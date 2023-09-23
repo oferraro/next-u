9e269bbdfd9e1f05f7b344a46873b30608a1b907
oferraro <oscar.ferraro@s4t.health>
2023-09-23 22:14:27 +0200

    Add sections in PHP to be able to reuse

---
+++ Docker/php/app/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+  include 'sections/head.php';
+?>
+
+<body>
+
+  <div class="hero_area">
+    <?php 
+      include 'sections/header.php';
+    ?>
+    <?php
+      include 'sections/slider.php';
+    ?>
+  </div>
+
+  <?php
+    include 'sections/service.php';
+  ?>
+
+  <?php
+    include 'sections/about.php';
+  ?>
+
+  <?php
+    include 'sections/project.php';
+  ?>
+
+  <?php
+    include 'sections/testimonial.php';
+  ?>
+
+  <?php
+    include 'sections/why-us.php';
+  ?>
+
+  <?php
+    include 'sections/contact.php';
+  ?>
+
+  <?php
+    include 'sections/info.php';
+  ?>
+
+  <?php
+    include 'sections/footer.php';
+  ?>
+
+  <!-- jQery -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- popper js -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.js"></script>
+  <!-- owl slider -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <!--  OwlCarousel 2 - Filter -->
+  <script src="https://huynhhuynh.github.io/owlcarousel2-filter/dist/owlcarousel2-filter.min.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+  <!-- Google Map -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+  </script>
+  <!-- End Google Map -->
+
+</body>
+
+</html>
